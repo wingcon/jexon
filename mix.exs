9698d045528f53092e2acbf3e033183f2eec388e
Kirill Meng <kirill.meng@wingcon.com>
2023-07-25 16:18:07 +0200

    fix mix.exs config 	- add ex_doc dep.

--- mix.exs
+++ mix.exs
@@ -9,10 +9,8 @@ defmodule Jexon.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Jexon is an Elixir library enabling a seamless connection between Elixir data structures and JSON, while maintaining unique Elixir types not directly supported in JSON",
-      licenses: ~w(MIT),
-      links: %{
-        "Github" => "https://github.com/wingcon/jexon"
-      }
+      description: description(),
+      package: package(),
     ]
   end
 
@@ -26,7 +24,23 @@ defmodule Jexon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.4.1"}
+      {:jason, "~> 1.4.1"},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Jexon is an Elixir library enabling a seamless connection between Elixir data structures and JSON, while maintaining unique Elixir types not directly supported in JSON
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: "wingcon",
+      licences: ~w(MIT),
+      links: %{"Github" => "https://github.com/wingcon/jexon"}
     ]
   end
 end
